b9311259ee8e852f0def575b48686fedeb2c100c
park-hwaan <parkhwan1129@gmail.com>
2025-12-19 14:31:22 +0900

    design: AboutMe관련 정보 하단에 추가

--- composeApp/src/commonMain/kotlin/com/example/hwanportfolio/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/hwanportfolio/App.kt
@@ -1,8 +1,11 @@
 package com.example.hwanportfolio
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,6 +13,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -32,63 +38,146 @@ import hwanportfolio.composeapp.generated.resources.image_title_background
 @Preview
 fun App() {
     MaterialTheme {
-        // 1. 전체를 위에서 아래로 배치하기 위한 최상위 Column 추가
-        Column(
+        LazyColumn(
             modifier = Modifier.fillMaxSize()
         ) {
-            // 2. 상단 배경 이미지 영역 (Box)
-            Box(modifier = Modifier.fillMaxWidth().height(400.dp)) {
-                Image(
-                    painter = painterResource(Res.drawable.image_title_background),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
+            item { TitleSection() }
+            item { Spacer(modifier = Modifier.height(40.dp)) }
+            item { AboutTitleSection() }
+            item { Spacer(modifier = Modifier.height(24.dp)) }
+        }
+    }
 
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .safeContentPadding(),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Spacer(modifier = Modifier.height(40.dp))
-                    Text(
-                        text = "박환\n안드로이드 개발자 포트폴리오",
-                        color = Color.Black,
-                        fontSize = 45.sp,
-                        lineHeight = 52.sp,
-                        textAlign = TextAlign.Center,
-                        fontFamily = FontFamily,
-                        fontWeight = FontWeight.SemiBold
-                    )
+}
 
-                    Spacer(modifier = Modifier.height(40.dp))
-                    Text(
-                        text = "안녕하세요.\n끊기있는 안드로이드 개발자\n박환입니다.",
-                        color = Color.Black,
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center,
-                        fontFamily = FontFamily,
-                        fontWeight = FontWeight.Thin
-                    )
-                }
+@Composable
+fun TitleSection() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.image_title_background),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .safeContentPadding(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(modifier = Modifier.height(80.dp))
+
+            Text(
+                text = "박환\n안드로이드 개발자 포트폴리오",
+                fontSize = 45.sp,
+                lineHeight = 52.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = FontFamily,
+                fontWeight = FontWeight.SemiBold
+            )
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            Text(
+                text = "안녕하세요.\n끈기있는 안드로이드 개발자\n박환입니다.",
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center,
+                fontFamily = FontFamily,
+                fontWeight = FontWeight.Thin
+            )
+        }
+    }
+}
+
+@Composable
+fun AboutTitleSection() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 60.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "ABOUT ME",
+            fontSize = 45.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Box(modifier = Modifier.width(250.dp).height(2.dp).background(Color.LightGray))
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Column(
+            modifier = Modifier
+                .widthIn(max = 1000.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 40.dp)
+        ) {
+            Row(modifier = Modifier.fillMaxWidth()) {
+                AboutItem("이름", "박환", Modifier.weight(1f))
+                AboutItem("생년월일", "00.11.29", Modifier.weight(1f))
+                AboutItem("주소", "서울특별시 강동구", Modifier.weight(1f))
             }
+            Spacer(modifier = Modifier.height(20.dp))
 
-            Spacer(modifier = Modifier.padding(top = 40.dp))
+            Row(modifier = Modifier.fillMaxWidth()) {
+                AboutItem("연락처", "010-7557-5986", Modifier.weight(1f))
+                AboutItem("이메일", "pahw001129@naver.com", Modifier.weight(1f))
+                AboutItem("학력", "한국공학대학교\n(컴퓨터공학부)", Modifier.weight(1f))
+            }
+        }
+    }
+}
 
-            Column(
+@Composable
+fun AboutItem(title: String, value: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+
+        Row(
+            modifier = Modifier.width(250.dp), // 글자 길이에 맞춰 180~220dp 사이로 조절하세요.
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+                    .size(36.dp)
+                    .background(Color.LightGray, shape = androidx.compose.foundation.shape.CircleShape)
+            )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column {
                 Text(
-                    text = "ABOUT ME",
-                    color = Color.Black,
-                    fontSize = 45.sp,
+                    text = title,
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
+                    color = Color.Black,
                     fontFamily = FontFamily
+                )
+                Text(
+                    text = value,
+                    fontSize = 15.sp,
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily,
+                    fontWeight = FontWeight.Medium,
+                    lineHeight = 18.sp
                 )
             }
         }
     }
 }
+
+
+
+
